4e5200e1d1adb98913176197e6e708bc35c1e2f0
farcy <farcyalexandre.60@hotmail.fr>
2020-10-03 03:46:19 +0200

    Ajout Script World rotation + zoom

---
+++ Assets/Alexandre/Script/WorldRotation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldRotation : MonoBehaviour
+{
+
+    public GameObject theWorld;
+
+    public bool  enablekey = false;
+
+    public Transform rotationWorld;
+   // Start is called before the first frame update
+    void Start()
+    {
+        theWorld = GameObject.FindGameObjectWithTag("theWorld");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enablekey == false)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                StartCoroutine(RotateMe(Vector3.up * 90, 0.3f));
+                enablekey = true;
+            }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                StartCoroutine(RotateMe(Vector3.up * -90, 0.3f));
+               enablekey = true;
+            }
+
+        }
+    }
+
+    IEnumerator RotateMe(Vector3 byAngles, float inTime)
+    {
+        var fromAngle = theWorld.transform.rotation;
+        var toAngle = Quaternion.Euler(theWorld.transform.eulerAngles + byAngles);
+        for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+        {
+            theWorld.transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+        enablekey = false;
+
+    }
+}
---
+++ Assets/Alexandre/Script/Zoom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour
+{
+
+    public float minFOV = 20f;
+    public float maxFOV = 80f;
+
+    public float sensibility = 10f;
+
+    // Start is called before the first frame update
+  
+    // Update is called once per frame
+    void Update()
+    {
+        float fov = Camera.main.fieldOfView;
+        fov -= Input.GetAxis("Mouse ScrollWheel") * sensibility;
+        fov = Mathf.Clamp(fov, minFOV, maxFOV);
+        Camera.main.fieldOfView = fov;
+    }
+}
