08bf3c680a6403aad454061c9a5528c5a43d97d1
Ronan <30617930+Ronan-senpai@users.noreply.github.com>
2020-10-04 19:33:48 +0200

    Main audio

---
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public static AudioManager Instance { get; set; }
+    private void Awake()
+    {
+        //Begin: Singleton
+        DontDestroyOnLoad(gameObject);
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+        //end
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
