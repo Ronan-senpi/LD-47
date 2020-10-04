368c3306a5554a5625fb0572f9668dcf754907e0
Ronan <30617930+Ronan-senpai@users.noreply.github.com>
2020-10-04 22:22:18 +0200

    Final du cul

--- Assets/Scripts/Player/PlayerDeplacement3D.cs
+++ Assets/Scripts/Player/PlayerDeplacement3D.cs
@@ -39,7 +39,11 @@ public class PlayerDeplacement3D : MonoBehaviour
     private Rigidbody rb;
     private Animator animator;
     private Projectil projectil;
-
+    private AudioSource audioWalk;
+    [SerializeField]
+    private AudioSource audiJump;
+    [SerializeField]
+    private AudioSource audiLand;
     // Start is called before the first frame update
     void Start()
     {
@@ -55,6 +59,10 @@ public class PlayerDeplacement3D : MonoBehaviour
         if (!TryGetComponent<Rigidbody>(out rb))
         {
             Debug.LogError("Need a Rigidbody");
+        }
+        if (!TryGetComponent<AudioSource>(out audioWalk))
+        {
+            Debug.LogError("Need a AudioSource");
         }
     }
     private void OnDrawGizmos()
@@ -72,12 +80,13 @@ public class PlayerDeplacement3D : MonoBehaviour
         this.direction = direction;
         moveAxis = axis;
     }
-
+    bool isJumping = false;
     private void FixedUpdate()
     {
         // Check if player is grounded
         isGrounded = Physics.OverlapBox(groundCheck.position, checkSize, transform.rotation, groundLayers)?.Length != 0;
         animator.SetBool("IsJumping", !isGrounded);
+
         //Move on X (lateral) axis
         moveInput = Input.GetAxisRaw(horizontalAxis);
         //Debug.Log(moveInput);
@@ -88,9 +97,13 @@ public class PlayerDeplacement3D : MonoBehaviour
             rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, direction * (moveInput * speed));
             //Debug.Log(new Vector3(rb.velocity.x, rb.velocity.y, direction * (moveInput * speed)));
         }
-        if (moveInput != 0)
+        if (moveInput != 0 && !audioWalk.isPlaying)
         {
-            AudioManager.Instance.Play("Walk");
+            audioWalk.Play();
+        }
+        else if(moveInput == 0)
+        {
+            audioWalk.Stop();
         }
         animator.SetFloat("Speed", Mathf.Abs(moveInput));
         if (!facingRight && moveInput > 0)
@@ -107,17 +120,27 @@ public class PlayerDeplacement3D : MonoBehaviour
     {
         if (isGrounded)
             extraJumpValue = extraJump;
-
+        //Land is not working
+        //if (IsGrounded && isJumping)
+        //{
+        //    isJumping = false;
+        //    audiLand.Play();
+        //}
         if (Input.GetButtonDown("Jump"))
         {
             Vector3 jumpVector = Vector3.up * jumpForce;
             if (extraJumpValue > 0) {
                 rb.AddForce(jumpVector, ForceMode.Impulse);
                 extraJumpValue--;
+                audiJump.Play();
+                isJumping = true;
             } else if (extraJump == 0 && isGrounded == true) {
                 rb.velocity = jumpVector;
+                audiJump.Play();
+                isJumping = true;
             }
         }
+
     }
 
     /// <summary>
