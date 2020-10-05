3f1c4474c4a0b82f46827b35c58502a08c27a02c
Ronan <30617930+Ronan-senpai@users.noreply.github.com>
2020-10-05 17:54:07 +0200

    Lvl1

---
+++ Assets/EndGameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameManager : MonoBehaviour
+{
+    [SerializeField]
+    string nextLvl;
+    [SerializeField]
+    LayerMask playerMask;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((playerMask.value & (1 << other.gameObject.layer)) > 0)
+        {
+            SceneManager.LoadScene(nextLvl);
+        }
+    }
+}
